ca7c40b56f353bac1279112b9cc3303729285741
lockermanwxlf <95518889+lockermanwxlf@users.noreply.github.com>
2022-08-01 10:25:32 -0500

    replaced names with keywords
    rather than using names (which i dont know) of items, i use keywords instead like the "straw" in "strawhat" or "straw_hat".

--- anime-adventures/merchant-sniper.lua
+++ anime-adventures/merchant-sniper.lua
@@ -1,27 +1,25 @@
 --lockermanwxlf#2293
-
-local items = {
-	StarFruit = "StarFruit";
-	StarFruitEpic = "StarFruitEpic";
-	LuckPotion = "LuckPotion";
+local keywords {
+	StarFruit = "starfruit";
+	LuckPotion = "luckpotion";
 	SummonTicket = "summon_ticket";
 	StarRemnant = "star_remnant";
-	StrawHat = "StrawHat";
-	MagmaFruit = "MagmaFruit";
+	StrawHat = "straw";
+	MagmaFruit = "magma";
 }
+
 local buying = {
-	items.StrawHat;
-	items.MagmaFruit;
-	items.SummonTicket;
-	items.StarFruit;
-	items.StarFruitEpic;
+	keywords.StrawHat;
+	keywords.MagmaFruit;
+	keywords.SummonTicket;
+	keywords.StarFruit;
 }
 local merchant = workspace:FindFirstChild("travelling_merchant", true)
 if merchant and merchant:FindFirstChild("is_open").Value == true then
 	local items = merchant.stand.items:GetChildren()
 	for i,v in pairs(items) do
 		for _, item in pairs(buying) do
-			if v.Name:find(item) then
+			if v.Name:lower():find(item) then
 				local A_1 = v.Name
 				local Event = game:GetService("ReplicatedStorage").endpoints["client_to_server"]["buy_travelling_merchant_item"]
 				Event:InvokeServer(A_1)
